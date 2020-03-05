6e4a1227105fd6fd7f1a69b76d4989d096457b42
Alec T. Musasa <alecmus@live.com>
2020-03-05 11:26:48 +0200

    Updated header

--- StudentSync_Client.cpp
+++ StudentSync_Client.cpp
@@ -1,9 +1,17 @@
 /*
-** SCS2206 Groupwork 2020
-** StudentSync Client
+** StudentSync LAN Synchronization Tool: Client
+**
+** This code may not be copied, modified or distributed without the
+** express written permission of the author(s). Any violation shall
+** be prosecuted to the maximum extent possible under law.
+**
+*************************************************************************
+** Project Details:
+**
+** National University of Science and Technology
+** SCS2206 Computing in Society Group Project
 **
 ** Group members: Alec T. Musasa, Kennedy J.J. Maturure
-**
 **
 */
 
