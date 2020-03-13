4c15239140307fe75e4f29427e9a4baa34329246
Alec T. Musasa <alecmus@users.noreply.github.com>
2020-03-13 18:49:54 +0200

    Update selectIP.h

--- selectIP.h
+++ selectIP.h
@@ -1,3 +1,24 @@
+/*
+** StudentSync LAN Synchronization Tool: Client
+** Copyright (c) 2020
+** Alec T. Musasa (alecmus at live dot com),
+** Kennedy J.J. Maturure (kenjjmat at outlook dot com)
+** 
+** Released under the Creative Commons Attribution Non-Commercial
+** 2.0 Generic license (CC BY-NC 2.0).
+** 
+** See accompanying file CC-BY-NC-2.0.txt or copy at [here](https://github.com/alecmus/StudentSync_Server/blob/master/CC-BY-NC-2.0.txt).
+**
+*************************************************************************
+** Project Details:
+**
+** National University of Science and Technology
+** SCS2206 Computing in Society Group Project
+**
+** Group members: Alec T. Musasa, Kennedy J.J. Maturure
+**
+*/
+
 #pragma once
 
 #include <string>
